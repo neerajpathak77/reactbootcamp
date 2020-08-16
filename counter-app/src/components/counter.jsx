96c02d4b7ef8095005ccb7b1317da61879f634ca
neeraj <>
2020-08-16 14:53:33 +0200

    Embedding Expressions ==> 003

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {  }
-  render() { 
-    // React.createElement('type') it needs type, so if multiple items are there babel does not know how to compile this long expression or you can which type of element this render should return. so in react we have to wrap elements to a parent
-    // Remember this div to convert to fragment
-    // Remember why parentheses - js places ; after return if no code
+  state = { 
+    count: 0
+  }
+
+  render() {
+
     return (
       <div>
-        <h1> Welcome !!</h1>
+        <span> {this.formatCount()}</span>
         <button>Increment</button>
       </div>
     )
+  }
+
+  formatCount = () => {
+    const { count } = this.state;
+
+    // Remember We can even return JSX like <span> Zero </span>
+    return count === 0 ? 'Zero' : count;
   }
 }
  
