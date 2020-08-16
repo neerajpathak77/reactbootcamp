a3d63ae07d2476a607b50e6c9dc0481dd5dd28f6
neeraj <>
2020-08-16 14:48:38 +0200

    Specifying Children ==> 002

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {  }
   render() { 
-    // React.createElement behind the scene thats why we have to import React
-    return <h1> Welcome !!</h1>;
+    // React.createElement('type') it needs type, so if multiple items are there babel does not know how to compile this long expression or you can which type of element this render should return. so in react we have to wrap elements to a parent
+    // Remember this div to convert to fragment
+    // Remember why parentheses - js places ; after return if no code
+    return (
+      <div>
+        <h1> Welcome !!</h1>
+        <button>Increment</button>
+      </div>
+    )
   }
 }
  
