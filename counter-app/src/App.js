d6fc1de233ab7e442254720ff84dbf3a3cd13fee
neeraj <>
2020-08-16 19:49:27 +0200

    Multiple Components in Sync ==> 023

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Counters from './components/counters';
+import NavBar from './components/navBar';
 
+// Starter Template https://getbootstrap.com/docs/4.5/components/navbar/
 function App() {
   return (
-    <Counters />
+    <>
+      <NavBar />
+      <Counters />
+    </>
   );
 }
 
