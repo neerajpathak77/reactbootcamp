105c16079fc142c0053b45342800f2bf037425f8
neeraj <>
2020-08-16 20:20:34 +0200

    Destructuring Arguments ==> 026

--- counter-app/src/components/navBar.jsx
+++ counter-app/src/components/navBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return ( 
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar
-        <span className="badge badge-pill badge-secondary m-2">{props.totalCounters}</span>
+        <span className="badge badge-pill badge-secondary m-2">{totalCounters}</span>
       </a>
     </nav>
     );
